d698568675674a7bdc91a903b6f1f93170bd428e
Chris Moynan <Chrismoynan@outlook.com>
2024-03-18 18:17:46 +0000

    updated

--- run.py
+++ run.py
@@ -1,112 +1,68 @@
 import random
 
-# Constants
-BOARD_SIZE = 5
-NUM_SHIPS = 3
-EMPTY = " "
-SHIP = "O"
-HIT = "X"
-MISS = "-"
+# List of words for the game
+WORDS = ["apple", "banana", "orange", "strawberry", "grape", "pineapple", "watermelon"]
 
+def choose_word(words):
+    """
+    Randomly choose a word from the list.
+    """
+    return random.choice(words)
 
-def initialize_board():
+def display_word(word, guessed_letters):
     """
-    Initialize an empty game board.
+    Display the word with guessed letters revealed and others hidden.
     """
-    return [[EMPTY] * BOARD_SIZE for _ in range(BOARD_SIZE)]
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+    return display.strip()
 
-
-def place_ships(board):
+def get_guess():
     """
-    Randomly place ships on the game board.
-    """
-    for _ in range(NUM_SHIPS):
-        row = random.randint(0, BOARD_SIZE - 1)
-        col = random.randint(0, BOARD_SIZE - 1)
-        board[row][col] = SHIP
-
-
-def print_board(player_board, computer_board):
-    """
-    Print the player's board and the computer's board with missed shots revealed.
-    """
-    print("   " + " ".join(str(i) for i in range(BOARD_SIZE)) + "       " + " ".join(str(i) for i in range(BOARD_SIZE)))
-    print("  " + "--" * BOARD_SIZE + "      " + "--" * BOARD_SIZE)
-    for i in range(BOARD_SIZE):
-        print(f"{i} | {' '.join(player_board[i])}      {' '.join(computer_board[i])}")
-
-def get_player_guess():
-    """
-    Get the player's guess for row and column.
+    Get a letter guess from the player.
     """
     while True:
-        try:
-            row = int(input("Enter row (0-4): "))
-            col = int(input("Enter column (0-4): "))
-            if 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE:
-                return row, col
-            else:
-                print("Invalid input! Please enter numbers between 0 and 4.")
-        except ValueError:
-            print("Invalid input! Please enter valid integers.")
+        guess = input("Guess a letter: ").lower()
+        if len(guess) == 1 and guess.isalpha():
+            return guess
+        else:
+            print("Invalid input! Please enter a single letter.")
 
-
-def get_computer_guess():
-    """
-    Generate a random guess for the computer.
-    """
-    row = random.randint(0, BOARD_SIZE - 1)
-    col = random.randint(0, BOARD_SIZE - 1)
-    return row, col
-
-
-def play_game():
+def play_game(word):
     """
     Main game loop.
     """
-    print("Welcome to Battleship!")
+    print("Welcome to Guess the Word!")
+    print("Try to guess the word within 6 attempts.")
 
-    # Initialize player and computer boards
-    player_board = initialize_board()
-    computer_board = initialize_board()
-    place_ships(player_board)
-    place_ships(computer_board)
+    guessed_letters = []
+    attempts = 6
 
-    # Game loop
-    while True:
-        # Player's turn
-        print("Player's turn:")
-        print_board(player_board, computer_board)
-        player_row, player_col = get_player_guess()
+    while attempts > 0:
+        print("\nWord:", display_word(word, guessed_letters))
+        print("Attempts left:", attempts)
+        
+        guess = get_guess()
 
-        if computer_board[player_row][player_col] == SHIP:
-            print("Hit!")
-            computer_board[player_row][player_col] = HIT
+        if guess in guessed_letters:
+            print("You already guessed that letter!")
+        elif guess in word:
+            print("Correct guess!")
+            guessed_letters.append(guess)
+            if set(guessed_letters) == set(word):
+                print("Congratulations! You guessed the word:", word)
+                break
         else:
-            print("Miss!")
-            computer_board[player_row][player_col] = MISS
+            print("Incorrect guess!")
+            attempts -= 1
 
-        # Check for player win
-        if check_game_over(computer_board):
-            print("Congratulations! You sunk all the computer's battleships!")
-            break
-
-        # Computer's turn
-        print("\nComputer's turn:")
-        computer_row, computer_col = get_computer_guess()
-
-        if player_board[computer_row][computer_col] == SHIP:
-            print("Computer hit your battleship!")
-            player_board[computer_row][computer_col] = HIT
-        else:
-            print("Computer missed!")
-            player_board[computer_row][computer_col] = MISS
-
-        # Check for computer win
-        if check_game_over(player_board):
-            print("Oops! The computer sunk all your battleships. Better luck next time!")
-            break
-
+    if attempts == 0:
+        print("Sorry, you ran out of attempts. The word was:", word)
 
 if __name__ == "__main__":
-    play_game()
+    word = choose_word(WORDS)
+    play_game(word)
